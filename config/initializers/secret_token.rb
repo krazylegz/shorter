f9ca3c2b56514329225b4d929519975a24dfff3f
Vikram Dighe <vikram@swiftsignal.com>
2012-09-08 10:05:12 -0400

    Initial Rails generation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shorter::Application.config.secret_token = 'f6f36b8f43fa5d2d147043180fd8e99bce10c55c60498baea91da6b1e7d63aaec984cc425983e5841268beb403bd500771fe4065588f35d210c4ebece43c222d'
