76dc72613aefa173bb3ffe064992e2c0468d9ca2
Vikram Dighe <vikram@swiftsignal.com>
2012-09-17 09:03:18 -0400

    Initial ShortUrl unit tests.

---
+++ spec/models/short_url_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe ShortUrl do
+	it 'should accept a long URL and provide the short URL'
+
+	it 'should accept a short URL and provide the short URL'
+
+	it 'should be repeatable'
+
+	it 'should differ by at least two characters from all other possible short URLs'
+
+	it 'should handle common transcription ambiguities'
+end
