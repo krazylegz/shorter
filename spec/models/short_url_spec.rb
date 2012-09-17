032201ee94dd341f9584650e55c339bacc24ce17
Vikram Dighe <vikram@swiftsignal.com>
2012-09-17 09:10:20 -0400

    Added unit tests for extra features.

--- spec/models/short_url_spec.rb
+++ spec/models/short_url_spec.rb
@@ -10,4 +10,10 @@ describe ShortUrl do
 	it 'should differ by at least two characters from all other possible short URLs'
 
 	it 'should handle common transcription ambiguities'
+
+	it 'should provide a management key when a short URL is created'
+
+	it 'should delete the short URL when a management key and short URL are given'
+
+	it 'should update the short URL when a managment key, old short URL and new short URL are given'
 end
